158056ce5a1a1cab0a29d9cd49039c0aadf59920
throwaway985 <77034560+throwaway985@users.noreply.github.com>
2021-01-05 21:37:01 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Jerma-Imposter-Message-Generator
-Generate your own imposter jerma barcode message
+Generate your very own jerma sus imposter message.
+
+Only supports the charecters " ", !, a, b, c, d, e, h, i, m, n, o, p, q, r, s, t, u, w and 😳.
+
+# Installation
+1. Download and install python from [python.org](https://www.python.org/)
+2. Download the main.py file
+
+# Usage
+1. Open up a command prompt/terminal wherever you downloaded the main.py file
+2. Run ```python main.py```
+3. Enter your message and watch the freaking magic happen baby
+
+Note: to use "😳" enter :flushed: into the message input.
---
+++ main.py
@@ -0,0 +1,111 @@
+from PIL import Image, ImageDraw
+import random
+
+# this codebase is FUCKED
+
+master_im = Image.open("imposter.jpg")
+master_string = "when the imposter is sus!"
+y_coord_split = 22
+master_x_dict = {
+    "w": [[7,29]],
+    "h": [[29,43], [83,98]],
+    "e": [[43,56], [98,111], [192,205]],
+    "n": [[56,69]],
+    " ": [[69,75], [111,116], [213,220], [238,244]],
+    "t": [[75,83], [183,192]],
+    "i": [[116,122], [220,225]],
+    "m": [[122,143]],
+    "p": [[143,157]],
+    "o": [[157,171]],
+    "s": [[171,183], [225,238], [244,257], [270,283]],
+    "r": [[205,213]],
+    "u": [[257,270]],
+    "!": [[282,289]],
+    "😳": [[289,312]],
+}
+
+# [should flip over x axis, should flip over y axis], [x1, x2]
+bootleg_x_dict = {
+    "a": [[True, False], [146, 155]],
+    "q": [[True, False], [143, 155]],
+    "b": [[False, True], [143, 157]],
+    "d": [[True, True], [143, 157]],
+    "c": [[False, False], [157,167]]
+}
+
+"""master_x_dict = {
+    "w": [7,1],
+    "h": [29,43],
+    "e": [43,56],
+    "n": [56,69],
+    " ": [69,75],
+    "t": [75,83],
+    "h": [83,98],
+    "e": [98,111],
+    " ": [111,116],
+    "i": [116,122],
+    "m": [122,143],
+    "p": [143,157],
+    "o": [157,171],
+    "s": [171,183],
+    "t": [183,192],
+    "e": [192,205],
+    "r": [205,213],
+    " ": [213,220],
+    "i": [220,225],
+    "s": [225,238],
+    " ": [238,244],
+    "s": [244,257],
+    "u": [257,270],
+    "s": [270,289],
+    "!": [282,289],
+    ":flushed:": [289,312]
+}"""
+
+input_string = input("Your message here: ").lower()
+input_string = input_string.replace(":flushed:", "😳")
+
+new_barcode = Image.new('RGB', (len(input_string)*12, master_im.height))
+total_width = 0
+
+emoticon_processing = False
+
+for i in input_string:
+    if i in master_x_dict.keys():
+        x_coords = master_x_dict[i][random.randint(0, len(master_x_dict[i]) - 1)]
+        scan_line = master_im.crop((x_coords[0], 0, x_coords[1], master_im.height))
+        new_barcode.paste(scan_line, (total_width, 0))
+        total_width += scan_line.width
+    elif i in bootleg_x_dict.keys():   
+        x_coords = bootleg_x_dict[i][1]
+        
+        letter = master_im.crop((x_coords[0], 0, x_coords[1], y_coord_split))
+        face = master_im.crop((x_coords[0], y_coord_split, x_coords[1], master_im.height))
+
+        # flip over x?
+        if bootleg_x_dict[i][0][0]:
+            letter = letter.transpose(Image.FLIP_LEFT_RIGHT)
+            face = face.transpose(Image.FLIP_LEFT_RIGHT)
+
+        # flip over y?
+        if bootleg_x_dict[i][0][1]:
+            letter = letter.transpose(Image.FLIP_TOP_BOTTOM)
+            face = face.transpose(Image.FLIP_TOP_BOTTOM)
+
+        # epic edge case
+        if i == "a":
+            draw = ImageDraw.Draw(letter)
+            draw.rectangle([5, 13, 8, 16], fill=(255,255,255,255))
+        
+
+        scan_line = Image.new('RGB', (letter.width, letter.height + face.height))
+        scan_line.paste(letter, (0, 0))
+        scan_line.paste(face, (0, y_coord_split))
+        new_barcode.paste(scan_line, (total_width, 0))
+        total_width += scan_line.width
+
+    else:
+        print(i + ": LETTER NOT SUPPORTED")
+    
+
+new_barcode.show()
