950466e940dc2012a8e02a0ee4a0c3a3442f97b1
throwaway985 <77034560+throwaway985@users.noreply.github.com>
2021-01-06 21:02:39 -0500

    added modes, refactored a lil bit, added build script

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
-# Jerma-Imposter-Message-Generator
+![](https://i.imgur.com/3VycXrp.png)
+#
+
 Generate your very own jerma sus imposter message.
 
-Only supports the charecters " ", !, a, b, c, d, e, h, i, m, n, o, p, q, r, s, t, u, w and 😳.
+
+# Modes:
+
+- **Default Mode**: Only supports the characters " ", !, a, b, c, d, e, h, i, m, n, o, p, q, r, s, t, u, w and 😳
+- **Cheater Mode**: Theoretically supports all characters, works by selecting a random slice of jerma's face then writing a letter above it
+- **Cypher Mode**: No letters, only barcode. One of the strongest encryption methods on the planet
 
 # Installation
-1. Download and install python from [python.org](https://www.python.org/)
-2. When installing make sure to check off "Add python to PATH"
-3. Download the main.py file
-4. Run ```pip install Pillow``` in a command prompt/terminal
+1. Download [this zip file](https://drive.google.com/file/d/1DGZ5i8UkFP7BW_cOeKnoinhWZW7ORClt/view?usp=sharing) and extract it
+2. I think you have to install the [Microsoft Visual C++ 2008 Redistributable Package](https://www.microsoft.com/en-us/download/details.aspx?id=29) although tbh I'm not sure if this is necesary
 
 # Usage
 1. Open up a command prompt/terminal wherever you downloaded the main.py file
 2. Run ```python main.py```
-3. Enter your message and watch the freaking magic happen baby
+3. Enter your message and parameters then watch the freaking magic happen baby
 
 Note: to use "😳" enter ```:flushed:``` into the message input.
+
+# Building
+1. Download repository
+2. Run ```pip install Pillow py2exe```
+3. Run ```python build.py py2exe``` in repository folder
+4. Move ```arial.ttf``` and ```imposter.jpg``` into ```dist/```
+5. To run the program, run main.exe in ```dist/```
 
 
 ![](https://i.imgur.com/txRMUHa.gif)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from PIL import Image, ImageDraw
-import random
+from PIL import Image, ImageDraw, ImageFont
+import random, os
 
 # this codebase is FUCKED
 
 master_im = Image.open("imposter.jpg")
 master_string = "when the imposter is sus!"
+
+# the y coordinate for where the text and the face split
 y_coord_split = 22
+
 master_x_dict = {
     "w": [[7,29]],
     "h": [[29,43], [83,98]],
@@ -32,6 +35,9 @@ bootleg_x_dict = {
     "d": [[True, True], [143, 157]],
     "c": [[False, False], [157,167]]
 }
+
+# list of all avalable charecters
+master_char_list = list(master_x_dict.keys()) + list(bootleg_x_dict.keys())
 
 """master_x_dict = {
     "w": [7,1],
@@ -62,50 +68,107 @@ bootleg_x_dict = {
     ":flushed:": [289,312]
 }"""
 
+# get input string
 input_string = input("Your message here: ").lower()
 input_string = input_string.replace(":flushed:", "😳")
 
-new_barcode = Image.new('RGB', (len(input_string)*12, master_im.height))
+# font creation for cheating mode
+font_path = (os.getcwd() + "\\arial.ttf")
+font = ImageFont.truetype(font_path, 13)
+
+# get yes/no and return as true/false
+def get_user_input_bool(question_string):
+    user_answer = input(question_string)
+    while user_answer not in ["y", "n"]:
+        user_answer = input(question_string)
+    
+    user_answer = True if user_answer == "y" else False
+    
+    return user_answer
+
+# yes or no cypher mode? no text, only jerma barcode
+cypher_mode = get_user_input_bool("Would you like to use cypher mode (y/n):")
+
+
+# yes or no cheating mode? Generate unsupported charecters with a random scan line
+cheating_mode = False
+for char in input_string:
+    if char not in master_char_list:
+        cheating_mode = get_user_input_bool("It looks like your using characters not in the default character set, would you like to activate cheating mode? (y/n):")
+        break
+
+# cypher warning
+change = False
+if cheating_mode and cypher_mode:
+    print("\nUsing cypher mode and cheating mode together is not reccomended due to")
+    print("the fact that the cheating mode uses an entirely random sliver of the image")
+    print("thus making it impossible to figure out the intended letter")
+    change_mode = get_user_input_bool("Would you like to disable cypher mode? (y/n)")
+    if change_mode:
+        cypher_mode = False
+
+final_image = Image.new('RGB', (len(input_string)*12, master_im.height))
+
+# keeps track of the total width of the image, the width of each strip is added at the end of each loop
 total_width = 0
 
-emoticon_processing = False
+for char in input_string:
+    if char in master_x_dict.keys():
+        x_coords = master_x_dict[char][random.randint(0, len(master_x_dict[char]) - 1)]
 
-for i in input_string:
-    if i in master_x_dict.keys():
-        x_coords = master_x_dict[i][random.randint(0, len(master_x_dict[i]) - 1)]
-        scan_line = master_im.crop((x_coords[0], 0, x_coords[1], master_im.height))
-        new_barcode.paste(scan_line, (total_width, 0))
-        total_width += scan_line.width
-    elif i in bootleg_x_dict.keys():   
-        x_coords = bootleg_x_dict[i][1]
+        letter = master_im.crop((x_coords[0], 0, x_coords[1], y_coord_split))
+        face = master_im.crop((x_coords[0], y_coord_split, x_coords[1], master_im.height))
+        
+    elif char in bootleg_x_dict.keys():   
+        x_coords = bootleg_x_dict[char][1]
         
         letter = master_im.crop((x_coords[0], 0, x_coords[1], y_coord_split))
         face = master_im.crop((x_coords[0], y_coord_split, x_coords[1], master_im.height))
 
         # flip over x?
-        if bootleg_x_dict[i][0][0]:
+        if bootleg_x_dict[char][0][0]:
             letter = letter.transpose(Image.FLIP_LEFT_RIGHT)
             face = face.transpose(Image.FLIP_LEFT_RIGHT)
 
         # flip over y?
-        if bootleg_x_dict[i][0][1]:
+        if bootleg_x_dict[char][0][1]:
             letter = letter.transpose(Image.FLIP_TOP_BOTTOM)
             face = face.transpose(Image.FLIP_TOP_BOTTOM)
 
         # epic edge case
-        if i == "a":
+        if char == "a":
             draw = ImageDraw.Draw(letter)
             draw.rectangle([5, 13, 8, 16], fill=(255,255,255,255))
         
+    elif cheating_mode:
+        random_x = random.randint(0, master_im.width-13)
+        scan_line_x_coords = [random_x, random_x+12]
+        
+        face = master_im.crop((scan_line_x_coords[0], y_coord_split, scan_line_x_coords[1], master_im.height))
 
-        scan_line = Image.new('RGB', (letter.width, letter.height + face.height))
-        scan_line.paste(letter, (0, 0))
-        scan_line.paste(face, (0, y_coord_split))
-        new_barcode.paste(scan_line, (total_width, 0))
-        total_width += scan_line.width
+        # create new blank letter template
+        letter = Image.new('RGB', (face.width, y_coord_split))
+        # draw white rectangle background and the letter
+        draw = ImageDraw.Draw(letter)
+        draw.rectangle([0, 0, letter.width, letter.height], fill=(255,255,255,255))
+        draw.text((0, 0), char, font=font, fill=(0,0,0,255))
+        # stretch the letter to match the natural stretch of the original image
+        letter = letter.resize((int(letter.width*1.75), letter.height))
+        letter = letter.crop((0, 0, face.width, letter.height))
 
     else:
-        print(i + ": LETTER NOT SUPPORTED")
-    
+        print(char + ": LETTER NOT SUPPORTED")
+        continue
 
-new_barcode.show()
+    # combine letter and face then paste it into the final image
+    scan_line = Image.new('RGB', (letter.width, letter.height + face.height))
+    scan_line.paste(letter, (0, 0))
+    scan_line.paste(face, (0, y_coord_split))
+    final_image.paste(scan_line, (total_width, 0))
+
+    total_width += scan_line.width
+    
+if cypher_mode:
+    final_image = final_image.crop((0, y_coord_split, final_image.width, final_image.height))
+
+final_image.show()
