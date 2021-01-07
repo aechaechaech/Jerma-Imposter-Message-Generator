b1a985a4878c9d671993104d43be63e6f4320ace
Thestickman391 <stickman391@gmail.com>
2021-01-07 21:52:43 +1300

    Oops

--- app.py
+++ app.py
@@ -217,4 +217,4 @@ def get_input_string(input_string, cypher_mode, cheating_mode):
 	
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
