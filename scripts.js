b2d5a544948ea8108c6e5ae239588cd558550d88
aechaechaech <77034560+aechaechaech@users.noreply.github.com>
2021-03-24 18:25:06 -0400

    default mode works now

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-﻿// the y coordinate for where the text and the face split
-const YCoordSplit = 22
+﻿const substringsArray = [' ','!', 'a', 'b', 'c', 'd', 'e', 'h', 'i', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', '😳', '򜰀🐀', '🐀򜰀', '򜰀', '🐀', '🐀'];
 
-const substringsArray = [' ','!', 'a', 'b', 'c', 'd', 'e', 'h', 'i', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', '😳', '򜰀🐀', '🐀򜰀', '򜰀', '🐀', '🐀'];
-
-const masterXDict = {
+let masterXDict = {
     "w": [[7,29]],
     "h": [[29,43], [83,98]],
     "e": [[43,56], [98,111], [192,205]],
@@ -30,14 +27,14 @@ const bootlegXDict = {
     "c": [[false, false], [157,167]]
 }
 
-//drawImage(image,
-//    sx, sy, sw, sh,
-//    dx, dy, dw, dh);
+// the y coord where text and face meet
+const yCoordSplit = 22;
 
 const canvas = document.getElementById("sustext");
 const ctx = canvas.getContext("2d");
 
-let currentMugshot = new Image();
+const susInput = document.getElementById("susInput");
+const errorOutput = document.getElementById("dumbassAlert");
 
 const templateImage = new Image();   
 templateImage.src = 'imposter.jpg';
@@ -46,11 +43,131 @@ templateImage.addEventListener('load', function() {
     canvas.width = templateImage.width;
     canvas.height = templateImage.height;
 
-    maxDiscWidth = canvas.width * 0.89;
+    canvas.style.width = "50%";
 
     ctx.drawImage(templateImage, 0, 0);
 })
 
+for (const property in masterXDict) {
+    let rand = Math.random();
+    rand *= masterXDict[property].length;
+    rand = Math.floor(rand);
+    
+    masterXDict[property] = masterXDict[property][rand]
+}
+
+function getWidth(susMsg) {
+    let totalWidth = 0;
+
+    for (const char of susMsg) {
+        let x_choords;
+
+        if (char in masterXDict) {
+            x_coords = masterXDict[char]
+        } else if (char in bootlegXDict) {
+            x_coords = bootlegXDict[char][1]
+        } else {
+            continue;
+        }
+
+        totalWidth += x_coords[1] - x_coords[0];
+    }
+
+    return totalWidth;
+}
+
+setInterval( function() {  
+    susMsg = susInput.value;
+    if (!susMsg) {
+        canvas.width = templateImage.width;
+        canvas.height = templateImage.height;
+
+        ctx.drawImage(templateImage, 0, 0);
+        canvas.style.width = `50%`;
+        errorOutput.innerHTML = "";
+        return;
+    }
+
+    susMsg = susMsg.toLowerCase();
+    susMsg = susMsg.split("");
+
+    // yell at user for using bad letters
+    
+    let processedMsg = [];
+    let unknownLetters = [];
+
+    for (const char of susMsg) {
+        if (substringsArray.includes(char)) {
+            processedMsg.push(char);
+        } else {
+            unknownLetters.push(char);
+        }
+    }
+
+    if (unknownLetters.length > 0) {
+        errorOutput.innerHTML = `Hey dumbass, default mode doesnt support ${unknownLetters}`;
+    } else {
+        errorOutput.innerHTML = "";
+    }
+
+    susMsg = processedMsg;
+
+    // get and set width, this is a dumb way to do it but i cant think of a better way to do it
+    let totalWidth = getWidth(susMsg);
+
+    const newWidth = (50*totalWidth)/320;
+    canvas.style.width = `${newWidth}%`;
+
+    canvas.width = totalWidth;
+
+    // make it
+    totalWidth = 0;
+    width = 0;
+    let flipArr = [];
+    for (const char of susMsg) {
+        if (char in masterXDict) {
+            x_coords = masterXDict[char]
+            width = x_coords[1] - x_coords[0]
+
+            ctx.drawImage(templateImage, x_coords[0], 0, width, canvas.height, totalWidth, 0 , width, canvas.height);
+        } else if (char in bootlegXDict) {
+            flipArr = bootlegXDict[char][0]
+            x_coords = bootlegXDict[char][1]
+
+            width = x_coords[1] - x_coords[0]
+            
+            // 1 if no flip, -1 if yes flip
+            // when doing flipping like this, you gotta make shit negative for whatever god forsaken reason
+            // go read the documentation man idk: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/scale
+            flipX = 1 - (2 * flipArr[0])
+            flipY = 1 - (2 * flipArr[1])
+
+            ctx.scale(flipX, flipY);
+
+            // draw letter
+            lCanvasX = flipX * totalWidth 
+            lCanvasY = 0
+            lCanvasWidth = flipX * width
+            lCanvasHeight = flipY * yCoordSplit
+            ctx.drawImage(templateImage, x_coords[0], 0, width, yCoordSplit, lCanvasX, lCanvasY, lCanvasWidth, lCanvasHeight);
+
+            //draw face
+            fCanvasX = flipX * totalWidth 
+            fCanvasY = flipY * yCoordSplit
+            fCanvasWidth = flipX * width
+            fCanvasHeight = flipY * canvas.height-yCoordSplit
+            ctx.drawImage(templateImage, x_coords[0], yCoordSplit, width, canvas.height-yCoordSplit, fCanvasX, fCanvasY, fCanvasWidth, fCanvasHeight);
+
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+        } else {
+            // theoretically this should never happen, but you can never be too sure
+            continue;
+        }
+
+        totalWidth += width;
+    }
+}, 60);
 
 function show_cheating() {
 	document.getElementById("options-help-cheating").style.display = "flex";
